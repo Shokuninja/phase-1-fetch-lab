e633ea7da7e2330cf2a0ffca30cee20218e505ef
Lohan <ltwm1998@gmail.com>
2023-10-05 16:20:42 -0400

    complete labs

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(response => response.json())
+  .then(myData => {
+      console.log(myData)
+      renderBooks(myData)
+      console.log(myData[4].name)
+
+    })
 }
 
 function renderBooks(books) {
